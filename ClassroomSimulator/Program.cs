a526a38c21a3a7d61e530dae9b5debd260de2459
Agnieszka Pyszka <aa.pyszka@gmail.com>
2022-10-23 09:44:40 +0200

    Added promt for user in ClassromSimulator

--- ClassroomSimulator/Program.cs
+++ ClassroomSimulator/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            Console.WriteLine("Podaj imie prowadzacego: ");
+            string teacherName = Console.ReadLine();
+            if(string.IsNullOrEmpty(teacherName))
+            {
+                Console.WriteLine("Imie jest nieprawidlowe");
+                return;
+            }
+            
         }
     }
 }
